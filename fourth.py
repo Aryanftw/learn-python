554c67075bd5fdff9eccdb47e0ba1062a6907cdf
Aryan <ap890383@gmail.com>
2025-07-21 11:54:38 +0530

    variables

---
+++ fourth.py
@@ -0,0 +1,15 @@
+#no variable declaration, dynamic typing
+name = "Aryan"
+print(name)
+#dynamic binding
+name = True
+print(name)
+
+a=5;b=6;c=7
+print(a,b,c)
+
+a,b,c = 5,6,7
+print(a,b,c)
+
+a=b=c=5
+print(a,b,c)
