f9c21451df0cfb5c7947ddc32f99b108863fb454
Aryan <ap890383@gmail.com>
2025-07-21 12:16:00 +0530

    took user input

---
+++ six.py
@@ -0,0 +1,9 @@
+input("Apna naam bata : ")
+#input is taken always in the string format
+first_num = input("first number : ")
+second_num = input("Second Number : ")
+print(first_num)
+print(second_num)
+print(type(first_num))
+print(type(second_num))
+print(int(first_num)+int(second_num))
