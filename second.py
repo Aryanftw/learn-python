114de1f71fd3027e92acd2b8e786cadfa28bf3f2
Aryan <ap890383@gmail.com>
2025-07-21 11:46:14 +0530

    datatypes intro in python

---
+++ second.py
@@ -0,0 +1,28 @@
+#3 types of datatypes in python : 
+#Basic : integer , float , complex , boolean , string
+#container : list , tuples , set , dictionary
+#user-defined : class
+
+
+#integer
+print(4)
+#float
+print(4.5)
+#boolean
+print(False)
+#complex
+print(4+5j)
+#string
+print("Hello")
+
+
+#containers
+
+#list
+print([1,2,3,4])
+#tuple
+print((1,2,3,4))
+#set
+print({1,2,3,4})
+#dictionary
+print({"name":"Aryan","age":19,"city" : "Surat"})
